9699d195f75e03e87ace8a2389cc77dc8bd16c2c
Bansie V Iyengar <54881314+Bansiev8@users.noreply.github.com>
2023-05-07 20:58:36 +0100

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section id="about">
+          <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                Hi, I'm Bansie.
+                <br className="hidden lg:inline-block" /> I build Stuff...
+              </h1>
+              <p className="mb-8 leading-relaxed">
+              I am a motivated, enthusiastic graduate with a strong interest in Cybersecurity. I would like to leverage
+                my knowledge in software development to drive innovation, by tackling challenge and complex
+                problems. My foundations in Cybersecurity, networking and emerging technologies will allow me to be
+                successful in range of roles. My curiosity leads me to explore the latest industry trends such as machine
+                learning, AI and Cloud computing and security. I enjoy working as part of a collaborative team as this
+                helps me to enhance my skills and consult with colleagues and contribute to the success of any
+                organisation.
+              </p>
+              <div className="flex justify-center">
+                <a
+                  href="#contact"
+                  className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  Work With Me
+                </a>
+                <a
+                  href="#projects"
+                  className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                  See My Past Work
+                </a>
+              </div>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+              <img
+                className="object-cover object-center rounded"
+                alt="hero"
+                src="./coding.svg"
+              />
+            </div>
+          </div>
+        </section>
+      );
+    
+}
